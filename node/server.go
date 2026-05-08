281b22deb0728c4847f976007886fb2497574a07
Kawahara <My.favorite.is.energy@gmail.com>
2026-05-08 20:20:11 +0900

    [feat] Introduce a Node struct

--- node/server.go
+++ node/server.go
@@ -7,38 +7,42 @@ import (
 	"net/url"
 	"net/http"
 	"net/http/httputil"
+
+	"github.com/MayugeStudio/lrucache"
 )
 
-const ORIGIN_SERVER_HOSTNAME = "origin"
+type Node struct {
+	 rp			*httputil.ReverseProxy
+	 cache  lrucache.LRUCache	
+}
 
-func main() {
-	nodeNumber, ok := os.LookupEnv("NODE_NUMBER")
-	if !ok {
-		panic("Failed to get NODE_NUMBER")
-	}
-	log.SetPrefix(fmt.Sprintf("[NODE%s] ", nodeNumber))
-
-	target, err := url.Parse("http://" + ORIGIN_SERVER_HOSTNAME + ":9000")
-	if err != nil {
-		panic("Failed to parse Origin server")
-	}
-	
+func NewNode(target *url.URL) *Node {
+	// Define rewrite function
 	rewrite := func(pr *httputil.ProxyRequest) {
+		// SetURL do the following things
+		// rewriteRequestURL(r.Out, target)
+		// r.Out.Host = ""
+
 		pr.SetURL(target)
 		pr.Out.Host = pr.In.Host
-		pr.SetXForwarded()
+
 		pr.Out.Header["X-Forwarded-For"] = pr.In.Header["X-Forwarded-For"]
+		pr.SetXForwarded()
 
-		log.Printf("%s -> %s\n", pr.In.RemoteAddr, target)
+		log.Printf("%s => %s\n", pr.In.RemoteAddr, target)
 	}
 
-	rp := &httputil.ReverseProxy{
-		Rewrite: rewrite,
+	return &Node{
+		rp: &httputil.ReverseProxy{
+			Rewrite: rewrite,
+		},
 	}
+}
 
+func (n *Node) Start(addr string) {
 	server := http.Server{
-		Addr: "0.0.0.0:5000",
-		Handler: rp,
+		Addr: addr,
+		Handler: n.rp,
 	}
 
 	hostname, err := os.Hostname()
@@ -49,3 +53,23 @@ func main() {
 	log.Printf("Start Listening at %s:9000\n", hostname)
 	log.Fatalln(server.ListenAndServe())
 }
+
+const ORIGIN_SERVER_HOSTNAME = "origin"
+
+func main() {
+	nodeNumber, ok := os.LookupEnv("NODE_NUMBER")
+	if !ok {
+		panic("Failed to get NODE_NUMBER")
+	}
+
+	log.SetPrefix(fmt.Sprintf("[NODE%s] ", nodeNumber))
+
+	target, err := url.Parse("http://" + ORIGIN_SERVER_HOSTNAME + ":9000")
+	if err != nil {
+		panic("Failed to parse Origin server")
+	}
+
+	node := NewNode(target)
+	node.Start("0.0.0.0:5000")
+}
+

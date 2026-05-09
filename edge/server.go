6ebf2996a0312ff673bbd75dc7bcd003a8bee336
Kawahara <My.favorite.is.energy@gmail.com>
2026-05-09 19:40:35 +0900

    [feat] Implement a function which generate a hash value from host, path and query parameters

--- edge/server.go
+++ edge/server.go
@@ -4,9 +4,13 @@ import (
 	"log"
 	"fmt"
 	"os"
+	"slices"
+	"maps"
 	"net/url"
 	"net/http"
 	"net/http/httputil"
+	"encoding/hex"
+	"crypto/sha256"
 
 	"github.com/MayugeStudio/lrucache"
 )
@@ -14,6 +18,31 @@ import (
 type Edge struct {
 	 rp			*httputil.ReverseProxy
 	 cache  lrucache.LRUCache	
+}
+
+// GenerateNewCacheKeyはキャッシュのキーをホスト、パス、クエリから作成する。
+// 作成する際、クエリパラメータの順番は無視する。
+// ホスト、パス、クエリの例: http://127.0.0.1:5001/index.html?test=hoge
+// ホスト: 127.0.0.1
+// パス  : /index.html
+// クエリ: ?test=hoge
+func GenerateNewCacheKey(host string, path string, queries map[string]string) string {
+	// sha256関数が[]byteを受け取るので、[]byteにhost, path, queriesを変換する
+	byteArray := make([]byte, len(host) + len(path) + len(queries)) // 正確ではないけど、一旦確保
+
+	// queriesの順番を無視するための処理。
+	// ?hoge=fuga&bar=baz
+	// の場合、
+	for _, k := range slices.Sorted(maps.Keys(queries)) {
+		byteArray = append(byteArray, []byte(k)...)
+		byteArray = append(byteArray, []byte(queries[k])...)
+	}
+
+	byteArray = append(byteArray, []byte(path)...)
+	byteArray = append(byteArray, []byte(host)...)
+
+	hashValue := sha256.Sum256(byteArray)
+	return hex.EncodeToString(hashValue[:])
 }
 
 // logger
@@ -25,20 +54,22 @@ func logger(next http.Handler) http.Handler {
 }
 
 func NewEdge(target *url.URL) *Edge {
-	// Define rewrite function
-	rewrite := func(pr *httputil.ProxyRequest) {
-		// SetURL do the following things
-		// rewriteRequestURL(r.Out, target)
-		// r.Out.Host = ""
+	// SetURL do the following things
+	// rewriteRequestURL(r.Out, target)
+	// r.Out.Host = ""
 
+	rewrite := func(pr *httputil.ProxyRequest) {
 		pr.SetURL(target)
 		pr.Out.Host = pr.In.Host
-
 		pr.Out.Header["X-Forwarded-For"] = pr.In.Header["X-Forwarded-For"]
 		pr.SetXForwarded()
-
 		log.Printf("%s => %s\n", pr.In.RemoteAddr, target)
+		log.Printf("  pr.In.URL.Query() => %s\n", pr.In.URL.Query())
+		log.Printf("  pr.In.URL.Path => %s\n", pr.In.URL.Path)
+		log.Printf("  pr.In.Host => %s\n", pr.In.Host)
 	}
+
+	// hash := GenerateNewCacheKey(pr.In.Host, pr.In.URL.Path, pr.In.URL.Query())
 
 	return &Edge{
 		rp: &httputil.ReverseProxy{

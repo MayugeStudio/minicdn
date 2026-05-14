49fb0910f45cd7a206d15d28276f1955ee8678bb
Kawahara <My.favorite.is.energy@gmail.com>
2026-05-14 20:27:04 +0900

    origin: serverのプログラムを更新(Chatgpt5)

--- origin/server.go
+++ origin/server.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
+	"sync"
 	"sync/atomic"
 	"time"
 )
 
 var counter uint64
+
+// ETag test content
+var (
+	contentMu sync.RWMutex
+	content   = "initial content"
+)
 
 func main() {
 	mux := http.NewServeMux()
@@ -20,16 +30,13 @@ func main() {
 	mux.HandleFunc("/private", cacheHandler("private"))
 	mux.HandleFunc("/must-revalidate", cacheHandler("public, max-age=5, must-revalidate"))
 
+	// ETag endpoint
+	mux.HandleFunc("/etag", etagHandler)
+
+	// Update ETag content
+	mux.HandleFunc("/update", updateHandler)
+
 	// Static file server
-	//
-	// Put files under:
-	// ./static/
-	//
-	// Example:
-	// ./static/image.png
-	//
-	// Access:
-	// http://localhost:9000/static/image.png
 	mux.Handle("/static/", http.StripPrefix(
 		"/static/",
 		http.FileServer(http.Dir("./static")),
@@ -56,6 +63,58 @@ func cacheHandler(cacheControl string) http.HandlerFunc {
 
 		json.NewEncoder(w).Encode(resp)
 	}
+}
+
+func etagHandler(w http.ResponseWriter, r *http.Request) {
+	contentMu.RLock()
+	currentContent := content
+	contentMu.RUnlock()
+
+	hash := sha1.Sum([]byte(currentContent))
+	etag := `"` + hex.EncodeToString(hash[:]) + `"`
+
+	// Revalidation
+	if r.Header.Get("If-None-Match") == etag {
+		w.WriteHeader(http.StatusNotModified)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Cache-Control", "public, max-age=5")
+	w.Header().Set("ETag", etag)
+
+	resp := map[string]interface{}{
+		"content": currentContent,
+		"etag":    etag,
+		"time":    time.Now().Format(time.RFC3339Nano),
+	}
+
+	json.NewEncoder(w).Encode(resp)
+}
+
+func updateHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	contentMu.Lock()
+	content = string(body)
+	contentMu.Unlock()
+
+	resp := map[string]interface{}{
+		"message": "content updated",
+		"content": content,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
 }
 
 func logging(next http.Handler) http.Handler {

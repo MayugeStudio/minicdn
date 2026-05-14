466a412395bb400b2dc7ce6c673eabc8958fbf71
Kawahara <My.favorite.is.energy@gmail.com>
2026-05-14 14:40:25 +0900

    [origin] Introduce new server program for testing cache control headers using chatgpt5

--- origin/server.go
+++ origin/server.go
@@ -1,25 +1,71 @@
 package main
 
 import (
-	"io"
+	"encoding/json"
 	"log"
 	"net/http"
+	"sync/atomic"
+	"time"
 )
 
-const hostname = "origin"
+var counter uint64
 
 func main() {
-	log.SetPrefix("[ORIGIN] ")
+	mux := http.NewServeMux()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Got a request\n")
-		log.Printf("	Sender IP (Previous Hop IP): %s\n", r.RemoteAddr)
-		log.Printf("	X-Forwarded-For: %s\n", r.Header.Get("X-Forwarded-For"))
+	// Cache-Control test endpoints
+	mux.HandleFunc("/max-age", cacheHandler("public, max-age=10"))
+	mux.HandleFunc("/no-store", cacheHandler("no-store"))
+	mux.HandleFunc("/no-cache", cacheHandler("no-cache"))
+	mux.HandleFunc("/private", cacheHandler("private"))
+	mux.HandleFunc("/must-revalidate", cacheHandler("public, max-age=5, must-revalidate"))
 
-		io.WriteString(w, "Hello from Origin Server\n")
-		io.WriteString(w, "I can serve my html very well\n")
+	// Static file server
+	//
+	// Put files under:
+	// ./static/
+	//
+	// Example:
+	// ./static/image.png
+	//
+	// Access:
+	// http://localhost:9000/static/image.png
+	mux.Handle("/static/", http.StripPrefix(
+		"/static/",
+		http.FileServer(http.Dir("./static")),
+	))
+
+	log.Println("listening on :9000")
+
+	http.ListenAndServe(":9000", logging(mux))
+}
+
+func cacheHandler(cacheControl string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		count := atomic.AddUint64(&counter, 1)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Cache-Control", cacheControl)
+
+		resp := map[string]interface{}{
+			"path":          r.URL.Path,
+			"cache_control": cacheControl,
+			"request_count": count,
+			"time":          time.Now().Format(time.RFC3339Nano),
+		}
+
+		json.NewEncoder(w).Encode(resp)
+	}
+}
+
+func logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf(
+			"%s %s",
+			r.Method,
+			r.URL.Path,
+		)
+
+		next.ServeHTTP(w, r)
 	})
-
-	log.Printf("Start Listening at %s:9000", hostname)
-	log.Fatalln(http.ListenAndServe(":9000", nil))
 }
